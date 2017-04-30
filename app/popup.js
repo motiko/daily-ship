5ef7aeefc655d4264fc673caf4b105f770cc8e71
moti <moti@practitest.com>
2017-05-01 01:06:41 +0700

    styling + error handling

--- app/popup.js
+++ app/popup.js
@@ -1,3 +1,5 @@
+const $i = document.getElementById.bind(document)
+
 function AuthenticationError(message) {
   this.name = 'AuthenticationError';
   this.message = message || 'AuthenticationError';
@@ -5,8 +7,6 @@ function AuthenticationError(message) {
 }
 AuthenticationError.prototype = Object.create(Error.prototype);
 AuthenticationError.prototype.constructor = AuthenticationError;
-
-const $i = document.getElementById.bind(document)
 
 function* requestEvents(user_login, publicity, token){
   for(let page=1; page <= 10; page++){
@@ -19,48 +19,62 @@ function* requestEvents(user_login, publicity, token){
   return Promise.resolve('Finished')
 }
 
-chrome.storage.sync.get('ghat', ({ghat}) => {
-  if(!ghat) return
-  console.log(ghat)
-  $i('connect_to_gh').style.display = 'none'
-  fetch(`https://api.github.com/user?access_token=${ghat}`).then(user_response => {
-    if(user_response.status == 401) throw( new AuthenticationError('BadToken'))
-    return user_response.json()
-  }).then(userData => {
-        console.log(userData)
-        return Promise.all([...requestEvents(userData.login,'/public',ghat)]).then((allData) =>{
-          let allEvents = Array.prototype.concat(...allData.map(obj=> obj.response))
-          let pushEvents = allEvents.filter(event => event.type == "PushEvent")
-          let pushDates = pushEvents.map(pe => pe.created_at)
-          console.log(pushDates)
-          return pushDates
+function fetchDataFromGithub(){
+  chrome.storage.sync.get('ghat', ({ghat}) => {
+    if(!ghat) return
+    $i('connect_to_gh').classList.add('hidden')
+    $i('fetching_data').classList.remove('hidden')
+    fetch(`https://api.github.com/user?access_token=${ghat}`).then(user_response => {
+      if(user_response.status == 401) throw( new AuthenticationError('BadToken'))
+      return user_response.json()
+    }).then(userData => {
+          console.dir(userData)
+          $i('avatar').src= userData.avatar_url
+          $i('fetching_data').classList.add('hidden')
+          $i('github_data').classList.remove('hidden')
+          return Promise.all([...requestEvents(userData.login,'/public',ghat)]).then((allData) =>{
+            let allEvents = Array.prototype.concat(...allData.map(obj=> obj.response))
+            let pushEvents = allEvents.filter(event => event.type == "PushEvent")
+            let pushDates = pushEvents.map(pe => pe.created_at)
+            console.log(pushDates)
+            return pushDates
+          })
+        }).catch((error)=>{
+          if(error.message === "BadToken"){
+            chrome.storage.sync.remove('ghat', ()=>{
+                showAuthError('Invalid token. Probably been expired or revoked.')
+            })
+          }
+          console.error(error)
         })
-      }).catch((error)=>{
-        if(error.message === "BadToken")
-        chrome.storage.sync.remove('ghat', ()=>{
-            $i('connect_to_gh').style.display = 'block'
-            console.log('removed bad token')
-        })
-        console.log(error)
-      })
-})
+  })
+}
+
+window.onload = function(){
+  console.log(0)
+  fetchDataFromGithub()
+}
 
 function filterPushEvents(allData){
   return allData.map(obj=> obj.response).reduce( (acc, arr) => [...acc,...arr])
           //.filter(event => event.type = "PushEvent")
 }
 
-$i('connect_to_github').addEventListener('click', connect_to_github)
+$i('connect_to_github_btn').addEventListener('click', connect_to_github)
 
 function connect_to_github(){
-
+  $i('connect_to_gh').classList.toggle('hidden')
+  $i('connecting').classList.toggle('hidden')
   let redirect_uri =  `https://${chrome.runtime.id}.chromiumapp.org/github_cb`
   let github_endpoint = 'https://github.com/login/oauth/authorize'
   let github_oauth_url = `${github_endpoint}?client_id=${github_oauth.client_id}&redirect_uri=${redirect_uri}`
   chrome.identity.launchWebAuthFlow({url: github_oauth_url , interactive: true},
     function(redirect_url) {
-      if(!redirect_url){
+      let lastError = chrome.runtime.lastError
+      if(lastError ){
         console.error('oAuth error occured')
+        console.error(lastError)
+        showAuthError(lastError.message)
         return
       }
       let code = /code\=(\w+)/.exec(redirect_url)[1]
@@ -73,9 +87,21 @@ function connect_to_github(){
         mode: 'cors',
         body: JSON.stringify(Object.assign({code},github_oauth))
       }).then(response => response.json()).then(json => {
-        chrome.storage.sync.set({'ghat': json.access_token}, ()=>{
-          console.log('set token')
+            chrome.storage.sync.set({'ghat': json.access_token}, ()=>{
+            $i('connecting').classList.add('hidden')
+            $i('connect_to_gh').classList.add('hidden')
+            fetchDataFromGithub()
         })
+      }).catch((exception)=>{
+        console.error(exception)
+        showAuthError(`Exception occured during authentication : ${exception.message} (Find more details in developer console)`)
       })
-     })
+    })
+}
+
+function showAuthError(errorMsg){
+  $i('connecting').classList.add('hidden')
+  $i('connect_to_gh').classList.remove('hidden')
+  $i('error_msg').classList.remove('hidden')
+  $i('error_msg').innerHTML = `Authentication error occured :<br/> "${errorMsg}"<br/> Please try authorize again <br/>`
 }
