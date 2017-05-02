6cbf49913466195769f7812b059fa083373c416a
moti <moti@practitest.com>
2017-05-02 11:56:47 +0700

    before changing to messages
    moving data fetch to background script

--- app/background.js
+++ app/background.js
@@ -1,25 +1,118 @@
-let optionsTabId = ""
+// function setBadge(){
+//   chrome.storage.sync.get('chainLength',function({chainLength}){
+//     console.log(chainLength)
+//     if(chainLength) chrome.browserAction.setBadgeText({text:`${chainLength}`})
+//   })
+// }
+// setBadge()
+setInterval(fetchDataFromGithub, 360000)
+fetchDataFromGithub()
 
-function openOptionsTab() {
-  if(optionsTabId){
-    chrome.tabs.get(optionsTabId, (tab) => {
-      if(tab) chrome.tabs.update(tab.id, {active: true})
-      chrome.windows.getCurrent({}, (currentWindow) => {
-        if(tab.windowId != currentWindow.id){
-          chrome.windows.update(tab.windowId, {focused: true})
-        }
-      })
-    })
+const MILLISECONDS_IN_DAY = 86400000
+
+function* requestEvents(user_login, publicity, token){
+  for(let page=1; page <= 10; page++){
+    yield fetch(`https://api.github.com/users/${user_login}/events${publicity}
+                      ?page=${page}&access_token=${token}`.replace(/\s/g,''))
+                      .then(r => r.json()).then(response => {
+                          return Promise.resolve({response,page})
+                      })
   }
-  else{
-    chrome.tabs.create({'url': chrome.extension.getURL('popup.html'),
-      'selected': true}, (tab) => optionsTabId = tab.id);
+  return Promise.resolve('Finished')
+}
+
+function fetchDataFromGithub(){
+  chrome.storage.sync.get('ghat', ({ghat}) => {
+    if(!ghat) return
+    fetch(`https://api.github.com/user?access_token=${ghat}`).then(user_response => {
+      if(user_response.status == 401) throw('BadToken')
+      return user_response.json()
+    }).then(userData => {
+          chrome.storage.sync.set({'ghUserData': userData})
+          return Promise.all([...requestEvents(userData.login,'/public',ghat)]).then((allData) =>{
+            let pushDates = getUnborkenChain(getPushDates(allData))
+            let chainLength = pushDates.length
+            chrome.storage.sync.set({'chainLength': chainLength})
+            if(chainLength) chrome.browserAction.setBadgeText({text:`${chainLength}`})
+          })
+        }).catch((error)=>{
+          if(error.message === "BadToken"){
+            chrome.storage.sync.remove('ghat', ()=>{})
+          }
+          console.error(error)
+        })
+  })
+}
+
+function getPushDates(allData){
+  let allEvents = Array.prototype.concat(...allData.map(obj=> obj.response))
+  let pushEvents = allEvents.filter(event => event.type == "PushEvent")
+  return pushEvents.map(pe => pe.created_at)
+}
+
+function getUnborkenChain(pushDates){
+  let today = new Date()
+  today.setHours(0,0,0,0)
+  pushDates = pushDates.sort((d1,d2) => new Date(d2) - new Date(d1))
+  pushDates = pushDates.map((d) => d.split('T')[0])
+  pushDates = pushDates.filter((val, i, arr) => arr.indexOf(val) === i)
+  let chain = pushDates.filter(unbrokenChainFrom(today))
+  if(chain.length == 0){
+    let yesterday = today - MILLISECONDS_IN_DAY
+    chain =  pushDates.filter(unbrokenChainFrom(yesterday))
+  }
+  return chain
+}
+
+function unbrokenChainFrom(from){
+  return function(date,index){
+    let startOfDay = new Date(date)
+    startOfDay.setHours(0,0,0,0)
+    return ( from - startOfDay ) / MILLISECONDS_IN_DAY == index
   }
 }
 
-chrome.browserAction.onClicked.addListener(openOptionsTab)
-
-chrome.tabs.onRemoved.addListener((tabId, changeInfo, tab) => {
-  if(optionsTabId == tabId)
-    optionsTabId = ""
+chrome.runtime.onMessage.addListener( (request) => {
+  switch(request.command){
+    case "connect_to_github":
+      connectToGithub()
+      break
+  }
 })
+
+
+function connectToGithub(){
+  let redirect_uri =  `https://${chrome.runtime.id}.chromiumapp.org/github_cb`
+  let github_endpoint = 'https://github.com/login/oauth/authorize'
+  let github_oauth_url = `${github_endpoint}?client_id=${github_oauth.client_id}&redirect_uri=${redirect_uri}`
+  chrome.identity.launchWebAuthFlow({url: github_oauth_url , interactive: true},
+    function(redirect_url) {
+      let lastError = chrome.runtime.lastError
+      if(lastError ){
+        console.error('oAuth error occured')
+        console.error(lastError)
+        showAuthError(lastError.message)
+        return
+      }
+      let code = /code\=(\w+)/.exec(redirect_url)[1]
+      chrome.storage.sync.set({'ghc': code})
+      let headers = new Headers({
+              "Content-Type": "application/json",
+              "Accept": "application/json"})
+      fetch("https://github.com/login/oauth/access_token", {
+        method: "POST",
+        headers,
+        mode: 'cors',
+        body: JSON.stringify(Object.assign({code},github_oauth))
+      }).then(response => response.json()).then(json => {
+            chrome.storage.sync.set({'ghat': json.access_token})
+      }).catch((exception)=>{
+        console.error(exception)
+        showAuthError(`Exception occured during authentication : ${exception.message} (Find more details in developer console)`)
+      })
+    })
+}
+
+function showAuthError(errorMsg){
+  console.error(errorMsg)
+}
--- app/popup.js
+++ app/popup.js
@@ -9,40 +9,50 @@ AuthenticationError.prototype = Object.create(Error.prototype);
 AuthenticationError.prototype.constructor = AuthenticationError;
 
 function* requestEvents(user_login, publicity, token){
+  let drawRect = drawRectFunc()
   for(let page=1; page <= 10; page++){
     yield fetch(`https://api.github.com/users/${user_login}/events${publicity}
                       ?page=${page}&access_token=${token}`.replace(/\s/g,''))
                       .then(r => r.json()).then(response => {
-                            return Promise.resolve({response,page})
+                          drawRect(null,page-1)
+                          return Promise.resolve({response,page})
                       })
   }
   return Promise.resolve('Finished')
 }
 
+chrome.storage.onChanged.addListener(function(changes,area){
+  if(area != "sync") return
+
+})
+
 function fetchDataFromGithub(){
   chrome.storage.sync.get('ghat', ({ghat}) => {
-    if(!ghat) return
+    if(!ghat){
+      $i('connect_to_gh').classList.remove('hidden')
+      $i('fetching_data').classList.add('hidden')
+      return
+    }
     $i('connect_to_gh').classList.add('hidden')
     $i('fetching_data').classList.remove('hidden')
     fetch(`https://api.github.com/user?access_token=${ghat}`).then(user_response => {
       if(user_response.status == 401) throw( new AuthenticationError('BadToken'))
       return user_response.json()
     }).then(userData => {
-          console.dir(userData)
+          chrome.storage.sync.set({'ghUserData': userData})
           $i('avatar').src= userData.avatar_url
           $i('fetching_data').classList.add('hidden')
           $i('github_data').classList.remove('hidden')
           return Promise.all([...requestEvents(userData.login,'/public',ghat)]).then((allData) =>{
-            let allEvents = Array.prototype.concat(...allData.map(obj=> obj.response))
-            let pushEvents = allEvents.filter(event => event.type == "PushEvent")
-            let pushDates = pushEvents.map(pe => pe.created_at)
-            console.log(pushDates)
-            return pushDates
+            clearRects()
+            let pushDates = getUnborkenChain(getPushDates(allData))
+            chrome.storage.sync.set({'chainLength': pushDates.length})
+            pushDates.forEach(drawRectFunc())
           })
         }).catch((error)=>{
           if(error.message === "BadToken"){
             chrome.storage.sync.remove('ghat', ()=>{
-                showAuthError('Invalid token. Probably been expired or revoked.')
+                showAuthError('Invalid token. Probably have been expired or revoked.')
             })
           }
           console.error(error)
@@ -50,14 +60,48 @@ function fetchDataFromGithub(){
   })
 }
 
-window.onload = function(){
-  console.log(0)
-  fetchDataFromGithub()
+function getPushDates(allData){
+  let allEvents = Array.prototype.concat(...allData.map(obj=> obj.response))
+  let pushEvents = allEvents.filter(event => event.type == "PushEvent")
+  return pushEvents.map(pe => pe.created_at)
 }
 
-function filterPushEvents(allData){
-  return allData.map(obj=> obj.response).reduce( (acc, arr) => [...acc,...arr])
-          //.filter(event => event.type = "PushEvent")
+
+function getUnborkenChain(pushDates){
+  pushDates = pushDates.sort(dateFns.compareDesc)
+  pushDates = pushDates.map((d) => dateFns.format(d,"YYYY-MM-DD"))
+  pushDates = pushDates.filter((val, i, arr) => arr.indexOf(val) === i)
+  var today = new Date()
+  pushDates = pushDates.filter( function( date, index){
+    return dateFns.differenceInCalendarDays(today, date) == index
+  })
+  return pushDates
+}
+
+function clearRects(){
+  let svg = $i('contributions')
+  svg.innerHTML = `<rect x="2" y="2" width="10" height="10" stroke="green" fill="green" fill-opacity="0.5" stroke-opacity="0.8" id="contrib-rect-proto" class="hidden contrib-rect"></rect>`
+}
+
+function drawRectFunc(){
+  let svg = $i('contributions')
+  return function(date,index){
+    let rowNum = (index % 12)
+    let colNum = (Math.floor(index / 12))
+    let rect = $i('contrib-rect-proto').cloneNode()
+    rect.classList.remove('hidden')
+    rect.removeAttribute('id')
+    let x =  Number.parseInt(rect.getAttribute('x'), 10)
+    let y =  Number.parseInt(rect.getAttribute('y'), 10)
+    let width = Number.parseInt(rect.getAttribute('width'), 10)
+    rect.setAttribute('x', x + (rowNum * (x + width)))
+    rect.setAttribute('y', y + (colNum * (y + width)))
+    svg.appendChild(rect)
+  }
+}
+
+window.onload = function(){
+  fetchDataFromGithub()
 }
 
 $i('connect_to_github_btn').addEventListener('click', connect_to_github)
@@ -101,6 +145,8 @@ function connect_to_github(){
 
 function showAuthError(errorMsg){
   $i('connecting').classList.add('hidden')
+  $i('fetching_data').classList.add('hidden')
+  $i('github_data').classList.add('hidden')
   $i('connect_to_gh').classList.remove('hidden')
   $i('error_msg').classList.remove('hidden')
   $i('error_msg').innerHTML = `Authentication error occured :<br/> "${errorMsg}"<br/> Please try authorize again <br/>`
